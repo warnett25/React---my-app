41e49f2d75a36a222e9efd3c3f345e89c08d401f
Anton Porokh <warnett25@gmai.com>
2023-04-19 09:53:37 +0600

    добавлены handleincrement и handledecrement

---
+++ src/components/counter.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react"
+
+const Counter = (props) => {
+  const { value, onIncrement } = props
+
+  const formatValue = () => {
+    return value === 0 ? "empty" : value
+  }
+
+  const getBageClasses = () => {
+    let classes = "badge m-2 "
+    classes += value === 0 ? "bg-warning" : "bg-primary"
+    return classes
+  }
+
+  // const handleIncrement = () => {
+  //   // setValue((prevState) => prevState + 1)
+  //   console.log("handleIncrement")
+  // }
+
+  // const handleDecrement = () => {
+  //   // setValue((prevState) => prevState - 1)
+  //   console.log("handleDecrement")
+  // }
+
+  return (
+    <div>
+      <span>{props.name}</span>
+      <span className={getBageClasses()}>{formatValue()}</span>
+
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => props.onDecrement(props.id)}
+      >
+        -
+      </button>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => props.onIncrement(props.id)}
+      >
+        +
+      </button>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => props.onDelete(props.id)}
+      >
+        Delete
+      </button>
+    </div>
+  )
+}
+
+export default Counter
